f7737f13e779c0cf9f37f4727e5199f10ee37e36
Sasikaran Kumaraiya <sasikarankumaraiya@gmail.com>
2025-11-17 19:17:24 +0530

    Update environment.ts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://206.189.159.222:5000', // Update with your production API URL
+  apiUrl: 'https://api.innovators.rocks',
   googleClientId: '732717088288-vrbfddmegpr076h0e9f7lear2ibdldhr.apps.googleusercontent.com',
   astrologyApi: {
     userId: '',
