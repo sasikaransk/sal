56d4c9152bc7e93fd501758111a9a233a6375a9a
Sasikaran Kumaraiya <sasikarankumaraiya@gmail.com>
2025-11-17 19:18:25 +0530

    Update environment.development.ts

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://206.189.159.222:5000', // Your ASP.NET backend URL
+  apiUrl: 'https://api.innovators.rocks', // Your ASP.NET backend URL
   googleClientId: '732717088288-vrbfddmegpr076h0e9f7lear2ibdldhr.apps.googleusercontent.com',
-  apiBaseUrl: 'http://206.189.159.222:5000/api', // your backend
+  apiBaseUrl: 'https://api.innovators.rocks/api', // your backend
   astrologyApi: {
     userId: 'YOUR_USER_ID', // Replace with your actual User ID
     apiKey: 'iohO1yjeUC75ZVz8bDdYL8YDr37VeYIl1oQKZ38a' // Replace with your actual API Key
